63fe5c3165e463b9d44d79ad09fa21fe39d3f848
iamvigneshwaran <vigneshwarana1511@gmail.com>
2023-01-10 23:10:27 +0530

    string logic in python

---
+++ string.py
@@ -0,0 +1,41 @@
+#string
+name = "vigneshwaran"
+
+#integer
+age = 23
+message = "how are you"
+print(name," how are you ,your age is =",age)
+print(name.upper())
+print(name.lower())
+
+print(message.title())
+
+firstname = "vignesh"
+lastname = "waran"
+
+fullname=firstname+lastname
+
+print(fullname.upper())
+print("\t")
+sum=0
+print(len(fullname)+age)
+
+#srore the boolean value
+
+paid = True  #first letter mattum caps la kudukanum
+
+print(name+" paid the money ",paid)
+
+#store the float value
+
+salary = 10000.00
+
+print(name+" salary=",salary)
+
+#char
+
+initial = 'A'
+print(initial)
+
+
+print(name+"hi "+salary+" "+name)
